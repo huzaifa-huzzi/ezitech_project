05490914a6285f424b101f6fe61bfdf87c1d571a
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-25 13:51:32 +0500

    1.Ezitech Project making

---
+++ lib/View/Attendance Showing.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AttendanceShowing extends StatefulWidget {
+  const AttendanceShowing({super.key});
+
+  @override
+  State<AttendanceShowing> createState() => _AttendanceShowingState();
+}
+
+class _AttendanceShowingState extends State<AttendanceShowing> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
