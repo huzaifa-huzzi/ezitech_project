000adee30c9bdc865a3d15d835c5d3130826d4b9
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-07-25 13:53:08 +0500

    1.Ezitech Project making

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: 'Ezitech Project',
+      debugShowCheckedModeBanner: false,
       home:LoginScreen(),
       getPages: AppRoutes.appRoutes(),
     );
